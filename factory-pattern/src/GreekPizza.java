fc40a14479769de7f183756cd9d76b2ebbbb8e61
ye-geeee <yeaji9683@gmail.com>
2021-05-31 06:26:58 +0900

    [factory] feat : add sample code that does not use factory pattern

---
+++ factory-pattern/src/GreekPizza.java
@@ -0,0 +1,5 @@
+public class GreekPizza extends Pizza {
+    public GreekPizza() {
+        name = "GreekPizza";
+    }
+}
---
+++ factory-pattern/src/PepperoniPizza.java
@@ -0,0 +1,5 @@
+public class PepperoniPizza extends Pizza {
+    public PepperoniPizza() {
+        name = "PepperoniPizza";
+    }
+}
