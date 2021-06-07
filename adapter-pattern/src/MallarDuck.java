ef8c5c74feaef6f1a5f2c774a3ef32e786273c3e
ye-geeee <yeaji9683@gmail.com>
2021-06-08 06:27:44 +0900

    [adapter] feat : add adapter pattern sample code

---
+++ adapter-pattern/src/MallarDuck.java
@@ -0,0 +1,11 @@
+public class MallarDuck implements Duck {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying");
+    }
+}
