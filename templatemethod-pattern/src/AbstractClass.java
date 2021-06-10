5439cd2864fd22502323f60be2a26a06bb75866a
ye-geeee <yeaji9683@gmail.com>
2021-06-11 05:44:33 +0900

    [templatemethod] feat : add template method pattern structure

---
+++ templatemethod-pattern/src/AbstractClass.java
@@ -0,0 +1,17 @@
+public abstract class AbstractClass {
+
+    final void templateMethod() {
+        primitiveOperation1();
+        primitiveOperation2();
+        concreteOperation();
+        hook();
+    }
+
+    abstract void primitiveOperation1();
+
+    abstract void primitiveOperation2();
+
+    final void concreteOperation() {}
+
+    void hook() {}
+}
