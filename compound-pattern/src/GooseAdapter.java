ac1c7b3748eb712b2fc5d6fac25bb6fe948a8c19
ye-geeee <yeaji9683@gmail.com>
2021-06-24 04:26:43 +0900

    feat: add simple compound pattern(adapter + strategy)

---
+++ compound-pattern/src/GooseAdapter.java
@@ -0,0 +1,12 @@
+public class GooseAdapter implements Quackable {
+    Goose goose;
+
+    public GooseAdapter(Goose goose) {
+        this.goose = goose;
+    }
+
+    @Override
+    public void quack() {
+        goose.honk();
+    }
+}
---
+++ compound-pattern/src/RedheadDuck.java
@@ -0,0 +1,6 @@
+public class RedheadDuck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
